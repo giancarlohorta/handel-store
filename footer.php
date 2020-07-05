62016ebcedfcbf31986b4be789a5e85a0de1be43
Giancarlo Horta <giancarlohorta@gmail.com>
2020-07-05 18:02:22 -0300

    slide and advantages

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
 
 
 <?php wp_footer(); ?>
+<script src ="<?= get_stylesheet_directory_uri(); ?>/js/slide.js"></script>
+<script src ="<?= get_stylesheet_directory_uri(); ?>/js/script.js"></script>
 </body>
 </html>
