79d306905cbf9411e0eca671b5a425c69b746052
Giancarlo Horta <giancarlohorta@gmail.com>
2020-07-06 16:03:42 -0300

    start products home

--- page-home.php
+++ page-home.php
@@ -2,30 +2,55 @@
 //Template name: Home
 get_header(); ?>
 
+<pre>
 <?php
+
+function format_products($products, $img_size){
+  $products_final = [];
+  foreach($products as $product){
+    $products_final[] = [
+      'name' => $product->get_name(),
+      'price' => $product->get_price_html(),
+      'link' => $product->get_permalink(),
+      'img' => wp_get_attachment_image_src($product->get_image_id(), $img_size)[0],
+    ];
+  }
+  return $products_final;
+}
 
 $produtos_slide = wc_get_products([
   'limit' => 6,
   'tag' => ['slide'],
 ]);
 
-function format_products($products){
-  $products_final = [];
-  foreach($products as $product){
-    $products_final[] = [
-      'name' => $product->get_name(),
-      'preco' => $product->get_price_html(),
-      'link' => $product->get_permalink(),
-      'img' => wp_get_attachment_image_src($product->get_image_id(), 'slide')[0],
-    ];
-  }
-  return $products_final;
-}
+$products_new = wc_get_products([
+  'limit' => 9,
+  'orderby' => 'date',
+  'order' => 'DESC'
+]);
 
-$slide = format_products($produtos_slide);
+$products_sales = wc_get_products([
+  'limit' => 9,
+  'meta_key' => 'total_sales',
+  'orderby' => 'meta_value_num',
+  'order' => 'DESC'
+]);
+
+
+$data = [];
+
+
+$data['slide'] = format_products($produtos_slide, 'slide');
+$data['lancamentos'] = format_products($products_new, 'medium');
+$data['vendidos'] = format_products($products_sales, 'medium');
+
+
+
+// print_r($data);
+
 
 ?>
-
+</pre>
 
 
 <?php if(have_posts()) { while(have_posts()) { the_post(); ?>
@@ -38,17 +63,28 @@ $slide = format_products($produtos_slide);
 
 <section class="slide-wrapper">
   <ul class="slide">
-    <?php foreach($slide as $product){?>
+    <?php foreach($data['slide'] as $product){?>
     <li class="slide-item">
     <img src="<?= $product['img'];?>" alt="<?= $product['name'];?>">
     <div class="slide-info">
-    <span class="slide-preco"><?= $product['preco'];?></span>
+    <span class="slide-preco"><?= $product['price'];?></span>
     <h2 class="slide-nome"><?= $product['name'];?></h2>
     <a class="slide-link" href="<?= $product['link'];?>">Ver Produto</a>
     </div>
     </li>
     <?php }?>
   </ul>
+</section>
+
+<section class="container">
+      <h1 class="subtitulo">Lançamentos</h1>
+
+      <?php handel_product_list($data['lancamentos'] )?>
+</section>
+
+<section class="container">
+      <h1 class="subtitulo">Mais Vendidos</h1>
+      <?php handel_product_list($data['vendidos'] )?>
 </section>
 
 <?php } } ?>
